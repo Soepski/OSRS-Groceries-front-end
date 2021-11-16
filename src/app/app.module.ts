f183b52d21909a150e6280895bfbd817788b6130
Ron Liebregts <ronlieb@hotmail.com>
2021-11-16 12:24:59 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OsrsSkillComponent } from './osrs-skill/osrs-skill.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OsrsSkillComponent
+    OsrsSkillComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
