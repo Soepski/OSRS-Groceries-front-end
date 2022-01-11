02b409ae21580c5620f07b98b2aab52641ec70aa
Ron Liebregts <ronlieb@hotmail.com>
2022-01-11 15:19:53 +0100

    Added chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 import { GroceryListComponent } from './grocery-list/grocery-list.component';
 import { OsrsSkillComponent } from './osrs-skill/osrs-skill.component';
 
 const routes: Routes = [
   { path: '', component: OsrsSkillComponent },
   { path: 'skills', component: OsrsSkillComponent },
-  { path: 'groceries', component: GroceryListComponent }
+  { path: 'groceries', component: GroceryListComponent },
+  { path: 'chat', component: ChatComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,32 +1,40 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { SignalrService } from '../signalr.service';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/chat.service';
+import { Message } from '../Models/message';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit, OnDestroy {
+export class ChatComponent implements OnInit {
 
-  constructor( 
-    public signalrService: SignalrService
-  ) 
-  {}
+  constructor(private chatService: ChatService) {}
 
-  someText:string = "";
-
-  ngOnInit() {
-    this.signalrService.startConnection();
-
-    setTimeout(() => {
-      this.signalrService.askServerListener()
-      this.signalrService.askServer();
-    }, 2000);
+  ngOnInit(): void {
+    this.chatService.retrieveMappedObject().subscribe( (receivedObj: Message) => { this.addToInbox(receivedObj);});  // calls the service method to get the new messages sent
+                                                     
   }
 
-  
-  ngOnDestroy() {
-    this.signalrService.hubConnection?.off("askServerResponse");
+  msgDto: Message = new Message();
+  msgInboxArray: Message[] = [];
+
+  send(): void {
+    if(this.msgDto) {
+      if(this.msgDto.user.length == 0 || this.msgDto.user.length == 0){
+        window.alert("Both fields are required.");
+        return;
+      } else {
+        this.chatService.broadcastMessage(this.msgDto);                   // Send the message via a service
+      }
+    }
   }
 
+  addToInbox(obj: Message) {
+    let newObj = new Message();
+    newObj.user = obj.user;
+    newObj.msgText = obj.msgText;
+    this.msgInboxArray.push(newObj);
+
+  }
 }
